44a81d3072dcdc7b6aab2ee1820afb7cbe9d902f
Thirumal <m.thirumal@hotmail.com>
2021-10-30 15:29:13 +0530

    Added working code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # s3-pre-signed-url
+
+### Install Dependencies
+
+    pip install -r requirements.txt
+
+### Run locally
+
+    chalice local
+
+### Deploy
+
+    chalice deploy
+
+### Replace the value with on following variable
+
+    1. bucket
+    2. aws_access_key_id
+    3. aws_secret_access_key
+    4. region_name
+    5. 
--- app.py
+++ app.py
@@ -1,10 +1,42 @@
+import logging
+import requests
+import json
+import os
+import boto3
+import logging
+
+from botocore.exceptions import ClientError
 from chalice import Chalice
 
-app = Chalice(app_name='presigned-url')
+app = Chalice(app_name='presigned-url', debug=True, configure_logs=False)
+
+s3_client = boto3.client('s3', region_name="ap-south-1", aws_access_key_id="${access_key}",
+                         aws_secret_access_key="${secret_key}")
+bucket = "${bucket_name}"
 
 
-@app.route('/generate-pre-signed-url-for-upload')
+@app.route('/generate-pre-signed-url-for-upload', methods=['POST'], content_types=['application/json'])
 def generate_pre_signed_url_for_upload():
-    return {'hello': 'world'}
+    logging.debug("Generate Pre-Signed URL for upload")
+    request = app.current_request
+    data = request.json_body
+    response = get_pre_signed_url(data['file_name'])
+    # files = {'file': open("/Users/thirumal/Downloads/ACC.png", 'rb')}
+    # print(files)
+    # http_response = requests.post(response['url'], data=response['fields'], files=files)
+    # print(http_response)
+    # If successful, returns HTTP status code 204
+    # logging.info(f'File upload HTTP status code: {http_response.text}')
+    return response
 
 
+def get_pre_signed_url(file_name):
+    try:
+        response = s3_client \
+            .generate_presigned_post(Bucket=bucket, Key=file_name, ExpiresIn=120)
+    except ClientError as e:
+        logging.error(e)
+        return None
+    logging.debug("Pre-Signed URL {}".format(response))
+    print(response)
+    return response
