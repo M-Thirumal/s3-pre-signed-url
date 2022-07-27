e424fd78501059b34a6fa0f972ef41d0eb9bc632
Thirumal <m.thirumal@hotmail.com>
2022-07-27 12:26:35 +0530

    url for both

--- app.py
+++ app.py
@@ -31,30 +31,41 @@ def handler(event, context):
     logging.debug("The event {} and context {}".format(event, context))
     version_id = event['arguments']['versionId']
     file_name = event['arguments']['fileName']
+    if 'fileEvent' in event['arguments']:
+        if event['arguments']['fileEvent'].lower() == 'download':
+            client_method = 'get_object'
+        else:
+            client_method = 'putObject'
+    else:
+        client_method = 'get_object'
     if 'bucket' in event['arguments']:
         logging.debug("Creating pre-signed url for the bucket {}".format(event['arguments']['bucket']))
-        return get_pre_signed_url(event['arguments']['bucket'], file_name, version_id)
-    return get_pre_signed_url(os.environ.get('bucket'), file_name, version_id)
+        return get_pre_signed_url(event['arguments']['bucket'], file_name, event['arguments']['fileEvent'], version_id)
+    return get_pre_signed_url(os.environ.get('bucket'), file_name, event['arguments']['fileEvent'], version_id)
 
 
-def get_pre_signed_url(bucket, file_name, version_id):
+def get_pre_signed_url(bucket, file_name, file_event, version_id):
     print("Creating pre-signed url for {}".format(bucket))
     try:
         # Don't use/depends on policy, "It will create pre-signed url but won't allow to upload the file
         # Will get "The AWS Access Key Id you provided does not exist in our records." error
         s3_client = boto3.client('s3', aws_access_key_id=os.environ.get("aws_access_key_id"), aws_secret_access_key=os
                                  .environ.get("aws_secret_access_key"), region_name=os.environ.get("region_name"))
-        # response = s3_client.generate_presigned_post(Bucket=bucket,
-        #                                              Key=os.environ.get('folder_location') + file_name,
-        #                                              ExpiresIn=300)
-        params = {
-            'Bucket': bucket,
-            'Key': os.environ.get('folder_location') + file_name
-        }
-        if version_id:
-            params['VersionId'] = version_id
-            logging.debug("Created url for version")
-        response = s3_client.generate_presigned_url(ClientMethod='get_object', Params=params)
+        key = os.environ.get('folder_location') + file_name
+        # Presigned url for upload
+        if file_event.lower() == 'upload':
+            response = s3_client.generate_presigned_post(Bucket=bucket, Key=key, ExpiresIn=300)
+            logging.debug("Created url for upload")
+        else:  # Presigned url for download
+            params = {
+                'Bucket': bucket,
+                'Key': key
+            }
+            if version_id:
+                params['VersionId'] = version_id
+                logging.debug("For Version")
+            response = s3_client.generate_presigned_url(ClientMethod='get_object', Params=params)
+            logging.debug("Created url for downloading")
     except ClientError as e:
         logging.error(e)
         return None
